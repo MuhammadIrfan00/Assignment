f7d46c9be5d8d61dcaad2e6b056fdabd96f5e2da
Muhammadirfan00 <afridiirfan00@gmail.com>
2021-07-25 23:35:47 +0500

    done three tasks

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-// // first q1
-// var itemsArray = [
-//     {name:'juice',price:'50',quantity:'3'},
-//     {name:'cookie',price:'30',quantity:'9'},
-//     {name:'shirt',price:'880',quantity:'1'},
-//     {name:'pen',price:'100',quantity:'2'}
-// ]
 
-// var each_item = document.getElementById('eachItem') 
-// var total_item = document.getElementById('totalItems')
-// var totalPrice = 0;
-// for(var i =0; i < itemsArray.length;i++){
-// // debugger
 
-//     var totalPriceEachItem =itemsArray[i].price * itemsArray[i].quantity 
-//     totalPrice += totalPriceEachItem
-//     console.log(totalPrice)
-// }
+var itemsArray = [
+    { name: 'juice', price: '50', quantity: '3' },
+    { name: 'juice', price: '30', quantity: '9' },
+    { name: 'juice', price: '880', quantity: '1' },
+    { name: 'juice', price: '100', quantity: '2' }
+]
 
-var available = document.getElementById('available')
+var each_item = document.getElementById('eachItem')
+var total_items = document.getElementById('totalItems')
+var totalPriceOfAllItem = 0
+for (var i = 0; i < itemsArray.length; i++) {
+    eachItem = parseInt(itemsArray[i].price) * parseInt(itemsArray[i].quantity)
+    totalPriceOfAllItem += eachItem
+    let node = document.createElement('LI');
+    var textNode = document.createTextNode(`Quantity : ${itemsArray[i].quantity} : Each price of item ${itemsArray[i].price} : Total price Of Item : ${eachItem} `);
+    node.appendChild(textNode)
+    each_item.appendChild(node)
+
+    total_items.innerHTML= `Total Price of All items : ${totalPriceOfAllItem}`
+}
+
+
+var obj = {name:'Irfan',email:'demo@gmail.com',password:'Start123',age:'26',gender:'male',city:'karachi',country:'Pakistan'}
+
+
+
 var obj = {
     name: 'muhammad Irfan',
     email: 'abc@gmail.com',
@@ -29,19 +37,28 @@ var obj = {
 }
 
 
-var ab = prompt().valueOf()
-for (var k in obj) {
-    if (k === ab) {
-        available.innerHTML=`this property is Available..! ${ab}`
-        break;
-    } else {
-        
-        available.innerHTML=`Sorry this property is not Available..! ${ab}`
-    }
+function checkProperty(){
 
+    var ab = prompt().valueOf()
+        if (obj[ab]) {
+            available.innerHTML=`this property is Available..! ${ab}`
+        } else {
+            
+            available.innerHTML=`Sorry this property is not Available..! ${ab}`
+        }
 }
 
 
 
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  }
+  
+  const person = new Person("John Doe", 23);
+  
+  console.log(`name: ${person.name} , age: ${person.age}`);
 
 
